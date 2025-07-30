1b014d60a801e6a3e9594b392d9cbb922f581660
Wei Nan Li <l.weinan@gmail.com>
2025-07-30 14:17:48 +0800

    add hello_kern.c

---
+++ hello_kern.c
@@ -0,0 +1,14 @@
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+// 定义许可证（必需）
+char _license[] SEC("license") = "GPL";
+
+// eBPF 程序：每次调用 sys_execve 时触发
+SEC("tracepoint/syscalls/sys_enter_execve")
+int hello(void *ctx) {
+    char msg[] = "Hello, eBPF World!";
+    bpf_trace_printk(msg, sizeof(msg)); // 输出到 trace_pipe
+    return 0;
+}
+
